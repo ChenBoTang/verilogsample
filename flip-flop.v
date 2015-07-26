15cf3c1de5b06552c478b255cf270e3df662d917
YOChenBoTang <happyday2593@gmail.com>
2015-07-26 19:14:42 +0800

    flip-flop

---
+++ flip-flop.v
@@ -0,0 +1,21 @@
+*
+   * D-flipflop module, just show off some verilog
+      */
+     module flipflop (
+		        input  clk,
+		        input  in,
+			output out);
+
+
+   reg 			       out_ff = 0;
+
+
+   assign out = out_ff;
+
+
+     always @ (posedge clk)
+       begin
+	  out_ff <= in;
+
+       end
+   endmodule
